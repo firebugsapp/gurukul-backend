d47514e5fe8fd22e63722e9eb56fdf51a5e00bdd
firebugsapp <firebugsapp@gmail.com>
2026-08-20 15:44:01 +0530

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -3,39 +3,54 @@ const mongoose = require("mongoose");
 const studentSchema = new mongoose.Schema(
   {
     studentId: {
-    type: String,
-    unique: true
-  },
+      type: String,
+      unique: true
+    },
+
     name: {
       type: String,
       required: true
     },
+
     fatherName: {
       type: String,
       required: true
     },
+
     className: {
       type: String,
       required: true
     },
+
     section: {
       type: String,
       required: true
     },
+
     rollNumber: {
       type: Number,
       required: true
     },
+
     mobile: {
       type: String,
       required: true
     },
+
     address: {
       type: String,
       required: true
+    },
+
+    // Firebase Cloud Messaging Token
+    fcmToken: {
+      type: String,
+      default: null
     }
   },
-  { timestamps: true }
+  {
+    timestamps: true
+  }
 );
 
 module.exports = mongoose.model("Student", studentSchema);
